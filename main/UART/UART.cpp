50d778d83bcd7a98dc7540ab82444af16fa4f029
Hbitz <53080862+Hbitz@users.noreply.github.com>
2026-05-15 18:03:39 +0200

    Cleaned up old comments and code

--- main/UART/UART.cpp
+++ main/UART/UART.cpp
@@ -4,13 +4,7 @@
 #include "freertos/task.h"
 #include "uart_diag_shell.hpp"
 #include "esp_log.h"
-//#include <stdbool.h>
-//#include <string.h>
 #include <stdio.h>
-// TODO - Should be extracted away from UART.
-#include "../leop/fake_leop.hpp"
-#include "../fake/fake_system_status.hpp"
-#include "../fake/fake_config.hpp"
 #include "esp_timer.h"
 #include "../app_types.h"
 #include "driver/uart.h"
@@ -19,7 +13,6 @@
 #define UART_BAUD 115200
 #define BUF_SIZE 1024
 
-//static app_state_t app;
 
 static const char* TAG = "UART";
 
@@ -47,78 +40,6 @@ std::string trim_copy(const std::string& str)
     return str.substr(start, end - start + 1);
 }
 
-
-
-extern "C" void UART_Init(app_state_t* app)
-{
-    ESP_LOGI(TAG, "UART diagnostics start, cpp mode.");
-    ESP_LOGI(TAG, "Type HELP for commands.");
-
-    // std::string input;
-    // std::string line;
-    // It seems that std::cin acts different than stdin.
-    // Raw C seems to be more reliable and stable than C++'s ways of handling inputs?
-    char input[8];
-    char line[128];
-    int line_pos = 0;
-    bool prompt_needed = true;
-
-    // Nu med fake producers
-    fake_system_status_fill(&app->system_status);
-    fake_config_data(&app->config_data);
-
-    fake_leop_fill(&app->leop_data);
-
-
-    static uint32_t last_fake_update = 0;
-    
-    while (1)
-    {
-        uint32_t now_seconds = esp_timer_get_time() / 1000000ULL;
-        
-        // Continously update our fake data to better mimic runtime behavior
-        // Our loops may run several times a second, but we can trottle it to only update our fake data once every new second
-        // TODO - Sensor fully extracted, rest are todo to be extracted away.
-        // UART should no interaction how fake data is produced or updated.
-        if (now_seconds != last_fake_update) {
-            last_fake_update = now_seconds;
-            fake_system_status_update(&app->system_status);
-        }
-
-        if (prompt_needed) {
-            printf("UART_DIAGNOSTICS_INTERFACE> ");
-            fflush(stdout);
-            prompt_needed = false;
-        }
-        
-
-        if (fgets(input, sizeof(input), stdin) != NULL) {  
-            for (int i = 0; input[i] != '\0'; i++) {
-                char c = input[i];
-                if (c == '\n' || c == '\r')
-                {
-                    line[line_pos] = '\0';
-                    ESP_LOGI(TAG, "Complete msg: %s", line);
-                    // trim and lowercase input.
-                    handle_input(to_lower_copy(trim_copy(line)), app);
-
-                    line_pos = 0;
-                    prompt_needed = true;
-                }
-                else {
-                    if (line_pos < sizeof(line) -1) {
-                        line[line_pos] = c;
-                        line_pos++;
-                    }
-                }
-            }
-
-        }
-        else {
-            vTaskDelay(pdMS_TO_TICKS(100));
-        }
-    }
-}
 
 void UART_Init_new(void)
 {
@@ -153,15 +74,10 @@ void UART_Init_new(void)
     ESP_LOGI(TAG, "UART%d initialized at %d baud", UART_PORT, UART_BAUD);
 }
 
-void UART_RX_TASK(void *args) {
-
-}
-
 
 extern "C" void UART_Work(void* parameter) {
     app_state_t* app = (app_state_t*)parameter;
 
-   // UART_Init(app);
     UART_Init_new();
 
    ESP_LOGI(TAG, "UART_Work started.");
@@ -177,15 +93,11 @@ extern "C" void UART_Work(void* parameter) {
    ESP_LOGI(TAG, "Uart WORK now entering main loop.");   
 
     while(1) {
-        // This for failstuff
-        //uint32_t now_seconds = esp_timer_get_time() / 1000000ULL;
         uint8_t byte;
         int len = uart_read_bytes(UART_PORT, &byte, 1, portMAX_DELAY);
 
-        if (len > 0)
-        {
-            if (byte == '\n' || byte == '\r')
-            {
+        if (len > 0) {
+            if (byte == '\n' || byte == '\r') {
                 line[line_pos] = '\0';
                 ESP_LOGI(TAG, "Complete msg: %s", line);
                 // trim and lowercase input.
@@ -197,22 +109,11 @@ extern "C" void UART_Work(void* parameter) {
             else {
                 if (line_pos < sizeof(line) -1) {
                     line[line_pos++] = byte;
-                    // line[line_pos] = c;
-                    // line_pos++;
                 }
             }
-
         }
         else {
             vTaskDelay(pdMS_TO_TICKS(100));
         }
-        // if (prompt_needed) {
-        //     printf("UART DIAGNOSTICS INTERFACE> ");
-        //     //fflush(stdout);
-        //     prompt_needed = false;
-        // }
-
     }
-
-
 }
