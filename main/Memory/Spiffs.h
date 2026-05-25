b64921570bed3da25732abd49bff8b55fe72a479
Elias.GS <kesebol.elias@telia.com>
2026-05-25 11:40:33 +0200

    Added support for spiffs in partition, Implemented an easy to use spiffs library that can later be used for caching

---
+++ main/Memory/Spiffs.h
@@ -0,0 +1,21 @@
+#ifndef SPIFFS_H
+#define SPIFFS_H
+
+#include "esp_spiffs.h"
+
+typedef struct{
+    esp_vfs_spiffs_conf_t spiffs_config;
+    char filepath[50];
+}Spiffs_t;
+
+
+int Spiffs_Initialize(Spiffs_t *spiffs, const char *file);
+
+int Spiffs_WriteToFile(Spiffs_t *spiffs, const char *content);
+
+int Spiffs_ReadFromFile(Spiffs_t *spiffs, char *buffer, size_t buffer_size);
+
+void Spiffs_Dispose(Spiffs_t* spiffs);
+
+
+#endif
